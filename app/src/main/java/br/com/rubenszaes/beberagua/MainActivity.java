e3c1b60afa125c25b5ce2da9611ebbd66ff8e710
RubensZaes <rubenszaes@gmail.com>
2020-11-27 08:51:39 -0300

    Iniciando a lógica do aplicativo

--- app/src/main/java/br/com/rubenszaes/beberagua/MainActivity.java
+++ app/src/main/java/br/com/rubenszaes/beberagua/MainActivity.java
@@ -3,12 +3,42 @@ package br.com.rubenszaes.beberagua;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnNotify;
+    private EditText editMinute;
+    private TimePicker timePicker;
+
+    private int hour;
+    private int minute;
+    private int interval;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnNotify = findViewById(R.id.btn_notify);
+        editMinute = findViewById(R.id.edit_txt_number_interval);
+        timePicker = findViewById(R.id.time_picker);
+
+        timePicker.setIs24HourView(true);
+
+    }
+
+    public void notifyClick(View view){
+        String sInterval = editMinute.getText().toString();
+        hour = timePicker.getCurrentHour();
+        minute = timePicker.getCurrentMinute();
+        interval = Integer.parseInt(sInterval);
+
+        Log.d("Teste", "Hora: " + hour + " Minute: " + minute + " Intervalo: " + interval);
     }
 }
